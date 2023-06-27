251852e0d3034efc77587f78bfae5b03e0e80e09
abdullahtaj <abdullahtaj@kubrickgroup.com>
2023-06-27 10:55:40 +0100

    updated snowflake details

--- tui_flight_booker.py
+++ tui_flight_booker.py
@@ -56,7 +56,7 @@ streamlit.header("View Our Fruit List - Add Your Favourites!")
 # snowflake related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
+        my_cur.execute("SELECT * from flights")
         return my_cur.fetchall()
 
 # Add a button to load the fruit data from Snowflake
@@ -68,7 +68,7 @@ if streamlit.button('Get Fruit Load List'):
 # Allow user to add fruits to the fruit table
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('{new_fruit}')")
+        my_cur.execute(f"insert into flights ('{new_fruit}')")
         return "Thanks for adding " + new_fruit
         
 
